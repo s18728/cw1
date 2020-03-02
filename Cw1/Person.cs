66c7c39a7970d2d01a161a3cb1cb323a7ea1c307
Jakub Oleksiak <s18728@pjwstk.edu.pl>
2020-03-02 09:57:17 +0100

    init

---
+++ Cw1/Person.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Cw1
+{
+    public class Person
+    {
+        public String FirstName { get; set; }
+    }
+}
---
+++ Cw1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Cw1
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            //string tmp2 = "Ala ma kota";
+            //string tmp3 = "i psa";
+            //int tmp4 = 4;
+            //Console.WriteLine($"{tmp2} {tmp3 + tmp4}");
+
+            //string path = @"C:\Users\s18728\Desktop\cw1";
+
+            //var tmp6 = 'c';
+            //var tmp7 = 2;
+            //var tmp8 = true;
+
+            ////int tmp9 = null;
+            //int? tmp9 = null;
+
+            //var newPerson = new Person { FirstName = "Jakub" };
+
+            var url = args[0];
+
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync(url))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var htmlContent = await response.Content.ReadAsStringAsync();
+
+                        var regex = new Regex(".+@.+[.].+", RegexOptions.IgnoreCase);
+                        var matches = regex.Matches(htmlContent);
+
+                        foreach (var match in matches)
+                        {
+                            Console.WriteLine(match.ToString());
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
